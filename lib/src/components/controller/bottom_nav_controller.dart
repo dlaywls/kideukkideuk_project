b8340d073a74d31235992b4fc0701f88f85f6512
dlaywls <gywls5184@naver.com>
2023-11-09 20:54:16 +0900

    feat: 화면 전환(뒤로가기버튼 까지) 기능 추가

--- lib/src/components/controller/bottom_nav_controller.dart
+++ lib/src/components/controller/bottom_nav_controller.dart
@@ -1,5 +1,46 @@
 import 'package:get/get.dart';
 
-class BottomNavController extends GetxController{
+enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
+
+class BottomNavController extends GetxController {
   RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
+
+  void changeBottomNav(int value, {bool hasGesture = true}) {
+    var page = PageName.values[value];
+    pageIndex(value);
+
+    switch (page) {
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.UPLOAD:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value, hasGesture: hasGesture);
+        break;
+    }
+  }
+
+  void _changePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+      print(bottomHistory);
+    }
+  }
+
+//뒤로가기 버튼 눌렀을 경우 기능
+  Future<bool> willPopAction() async {
+    if (bottomHistory.length == 1) {
+      print('exit');
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      var index = bottomHistory.last;
+      changeBottomNav(index, hasGesture: false);
+      print(bottomHistory);
+      return false;
+    }
+  }
 }
