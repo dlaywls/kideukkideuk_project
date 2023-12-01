c8a367e1836bea5f601e0f9354f08fb468c2004e
dlaywls <gywls5184@naver.com>
2023-12-01 18:40:22 +0900

    feat: splash화면, 회원가입 화면

--- lib/src/pages/signupPage.dart
+++ lib/src/pages/signupPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
 import 'package:kideukkideuk_project/src/controller/auth_controller.dart';
 import 'package:kideukkideuk_project/src/models/kideuk_user.dart';
 
@@ -12,42 +13,96 @@ class SignupPage extends StatefulWidget {
 
 class _SignupPageState extends State<SignupPage> {
   TextEditingController nicknameController = TextEditingController();
+  bool isNicknameFilled = false;
 
   Widget _nickname() {
-    return TextField(
-      controller: nicknameController,
-      decoration: const InputDecoration(
-          contentPadding: EdgeInsets.all(10), hintText: '닉네임'),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min, // mainAxisSize 설정 추가
+        children: [
+          const Text(
+            '닉네임을 입력해주세요',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ).marginOnly(top: 60, left: 5),
+          const SizedBox(height: 12),
+          const Text(
+            '다시 변경하기 어려우니 신중하게 입력해주세요',
+            style: TextStyle(fontSize: 12),
+            textAlign: TextAlign.center,
+          ).marginOnly(left: 5),
+          const SizedBox(height: 40),
+          Flexible(
+            // Flexible로 변경
+            child: TextField(
+              controller: nicknameController,
+              onChanged: (value) {
+                setState(() {
+                  isNicknameFilled = value.trim().isNotEmpty;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: '닉네임 입력하기',
+                contentPadding: const EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget _signupButton() {
+    return isNicknameFilled
+        ? Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                var signupUser =
+                    KUser(uid: widget.uid, nickname: nicknameController.text);
+                AuthController.to.signup(signupUser);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xFF52B160),
+                padding: const EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(22),
+                ),
+              ),
+              child: const Text('가입하기',
+                  style: TextStyle(fontSize: 16, color: Colors.white)),
+            ),
+          )
+        : const SizedBox(); // 텍스트 필드가 비어있으면 빈 SizedBox 반환
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        title: const Text('회원가입'),
-      ),
-      body: SingleChildScrollView(
-          child: Column(
+      appBar: null,
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const SizedBox(height: 20),
-          _nickname(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _nickname(),
+                  // 다른 필요한 위젯들 추가 가능
+                ],
+              ),
+            ),
+          ),
+          _signupButton(),
         ],
-      )),
-      bottomNavigationBar: Container(
-        child: ElevatedButton(
-          onPressed: () {
-            //validation
-            var signupUser =
-                KUser(uid: widget.uid, nickname: nicknameController.text);
-            AuthController.to.signup(signupUser);
-          },
-          child: const Text('회원가입'),
-        ),
       ),
     );
   }
